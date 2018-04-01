0e82fa9edf854e22b191f2871edc30fdbad3f796
Riin <nassii74@gmail.com>
2018-04-01 16:56:52 +0900

    [chore]bootstrap4

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,16 +5,18 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
 
 window.Vue = require('vue');
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
+Vue.use('BootstrapVue');
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
